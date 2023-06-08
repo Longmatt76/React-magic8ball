0c9ce7fc6b3e8638135f60dfd01131a3508a2344
Matthew Long <longm7676@gmail.com>
2023-06-08 14:30:16 -0500

    started project

---
+++ src/Eightball.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import answers from "./answers";
+import './Eightball.css';
+
+const EightBall = () => {
+    return (
+<>
+
+
+
+</>
+    )
+}
+
+
+export default EightBall;
